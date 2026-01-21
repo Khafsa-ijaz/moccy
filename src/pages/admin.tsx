c29e08b52a262635c581631dc8f4abd9f2d60ade
Khafsa Ijaz <khafsaijazijazkhan16@gmail.com>
2026-01-21 17:13:14 +0500

    new

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -2,7 +2,7 @@ export function Admin()
 {
     return(
         <>
-        <aside className="bg-blue-950 w-60 h-full">hello</aside>
+        <aside className="bg-blue-950 w-60 h-full text-white">hello</aside>
         <main className="bg-white flex-1 h-full"></main>
         </>
     )
