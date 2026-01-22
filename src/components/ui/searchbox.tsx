276c9e7cbea182b9d9e38ab8c094716edcc746a4
Khafsa Ijaz <khafsaijazijazkhan16@gmail.com>
2026-01-22 18:26:43 +0500

    thursday

--- src/components/ui/searchbox.tsx
+++ src/components/ui/searchbox.tsx
@@ -1,6 +1,7 @@
+import { FiSearch } from "react-icons/fi";
 export function Search()
 {
     return(
-        <></>
+        <div className=""><input type="text" placeholder="search or type a command here" className=" border borer-2 border-neutral-300 h-8 w-90 rounded-sm pl-10 relative"/><FiSearch  size={22}  className=" text-neutral-500 absolute top-5 left-75 "/></div>
     )
 }
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,9 +1,24 @@
-export function Admin()
-{
-    return(
-        <>
-        <aside className="bg-blue-950 w-60 h-full text-white">hello</aside>
-        <main className="bg-white flex-1 h-full"></main>
-        </>
+import moccylogo from "../assets/images/logo.png";
+import { RiMenuFold3Line } from "react-icons/ri";
+import { Search } from "../components/ui/searchbox";
+export function Admin() {
+    return (
+        <div className="flex">
+            <aside className="bg-blue-950 w-70 min-h-145 text-white">
+                <div className="flex items-center justify-between ">
+                    <div>
+                        <div className="flex items-center gap-0"><img src={moccylogo} alt='logo' className="w-25 h-25 object-contain relative" /><span className="text-3xl font-bold absolute left-20">MOCCY</span></div>
+                        <span className="text-xs font-medium absolute top-17 left-40">Admin</span>
+                    </div>
+                    <div>
+                        <RiMenuFold3Line size={30} className="fill-white mr-2"/>
+                    </div>
+
+                </div>
+            </aside>
+            <main className="bg-white flex-1 h-full p-4">
+                <Search/>
+            </main>
+        </div>
     )
 }
